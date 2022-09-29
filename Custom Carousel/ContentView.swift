b6f8f195a0b2773c53a410aa687b4c9774d46fc6
hazem-smawy <hazemsmawy@gmail.com>
2022-09-29 14:26:02 +0300

    Initial Commit

---
+++ Custom Carousel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Custom Carousel
+//
+//  Created by hazem smawy on 9/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Custom Carousel/Custom_CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_CarouselApp.swift
+//  Custom Carousel
+//
+//  Created by hazem smawy on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Custom_CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
